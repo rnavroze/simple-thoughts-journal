4cf8e2113c7e4cb8d90d32beeb6f2e0122113439
raihan <raihan@raitech.org>
2020-07-06 15:47:47 +0530

    Fix add new button to correctly show add/edit, add warning dialog when exiting add new entry page.

--- lib/new_entry.dart
+++ lib/new_entry.dart
@@ -28,147 +28,170 @@ class _AddNewEntryState extends State<AddNewEntry> {
     }
   }
 
-  void updateDistortions(Set distortionIds) {
+  void _updateDistortions(Set distortionIds) {
     setState(() {
       _journalEntry.distortions = distortionIds;
     });
   }
 
-  void updateHALT(Map halt) {
+  void _updateHALT(Map halt) {
     setState(() {
       _journalEntry.halt = halt;
     });
+  }
+
+  Future<bool> _onWillPop() async {
+    return (await showDialog(
+          context: context,
+          builder: (context) => new AlertDialog(
+            title: new Text('Warning'),
+            content: new Text('Are you sure you wish to go back without saving this entry?'),
+            actions: <Widget>[
+              new FlatButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: new Text('No'),
+              ),
+              new FlatButton(
+                onPressed: () => Navigator.of(context).pop(true),
+                child: new Text('Yes'),
+              ),
+            ],
+          ),
+        )) ??
+        false;
   }
 
   @override
   Widget build(BuildContext context) {
     final bottom = MediaQuery.of(context).viewInsets.bottom;
 
-    return Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          title: Text('Add New Entry'),
-        ),
-        body: SingleChildScrollView(
-            reverse: true,
-            child: Padding(
-                padding: EdgeInsets.only(bottom: bottom),
-                child: Container(
-                  padding: EdgeInsets.all(8.0),
-                  child: Form(
-                      key: _formKey,
-                      child: Column(
-                        children: <Widget>[
-                          CustomTextFormField(
-                            hintText: 'Title',
-                            value: _journalEntry.title,
-                            validator: (String value) {
-                              if (value.isEmpty) {
-                                return 'Enter title';
-                              }
-                              return null;
-                            },
-                            onSaved: (String value) {
-                              _journalEntry.title = value;
-                            },
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return WillPopScope(
+        onWillPop: _onWillPop,
+        child: Scaffold(
+            resizeToAvoidBottomInset: false,
+            appBar: AppBar(
+              title: Text('Add New Entry'),
+            ),
+            body: SingleChildScrollView(
+                reverse: true,
+                child: Padding(
+                    padding: EdgeInsets.only(bottom: bottom),
+                    child: Container(
+                      padding: EdgeInsets.all(8.0),
+                      child: Form(
+                          key: _formKey,
+                          child: Column(
                             children: <Widget>[
-                              IconButton(
-                                  icon: Icon(this._journalEntry.level == 1 ? Icons.looks_one : Icons.filter_1),
-                                  onPressed: () => setState(() => this._journalEntry.level = 1)),
-                              IconButton(
-                                  icon: Icon(this._journalEntry.level == 2 ? Icons.looks_two : Icons.filter_2),
-                                  onPressed: () => setState(() => this._journalEntry.level = 2)),
-                              IconButton(
-                                  icon: Icon(this._journalEntry.level == 3 ? Icons.looks_3 : Icons.filter_3),
-                                  onPressed: () => setState(() => this._journalEntry.level = 3)),
-                              IconButton(
-                                  icon: Icon(this._journalEntry.level == 4 ? Icons.looks_4 : Icons.filter_4),
-                                  onPressed: () => setState(() => this._journalEntry.level = 4)),
-                              IconButton(
-                                  icon: Icon(this._journalEntry.level == 5 ? Icons.looks_5 : Icons.filter_5),
-                                  onPressed: () => setState(() => this._journalEntry.level = 5)),
+                              CustomTextFormField(
+                                hintText: 'Title',
+                                value: _journalEntry.title,
+                                validator: (String value) {
+                                  if (value.isEmpty) {
+                                    return 'Enter title';
+                                  }
+                                  return null;
+                                },
+                                onSaved: (String value) {
+                                  _journalEntry.title = value;
+                                },
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  IconButton(
+                                      icon: Icon(this._journalEntry.level == 1 ? Icons.looks_one : Icons.filter_1),
+                                      onPressed: () => setState(() => this._journalEntry.level = 1)),
+                                  IconButton(
+                                      icon: Icon(this._journalEntry.level == 2 ? Icons.looks_two : Icons.filter_2),
+                                      onPressed: () => setState(() => this._journalEntry.level = 2)),
+                                  IconButton(
+                                      icon: Icon(this._journalEntry.level == 3 ? Icons.looks_3 : Icons.filter_3),
+                                      onPressed: () => setState(() => this._journalEntry.level = 3)),
+                                  IconButton(
+                                      icon: Icon(this._journalEntry.level == 4 ? Icons.looks_4 : Icons.filter_4),
+                                      onPressed: () => setState(() => this._journalEntry.level = 4)),
+                                  IconButton(
+                                      icon: Icon(this._journalEntry.level == 5 ? Icons.looks_5 : Icons.filter_5),
+                                      onPressed: () => setState(() => this._journalEntry.level = 5)),
+                                ],
+                              ),
+                              CustomTextFormField(
+                                hintText: 'Details',
+                                value: _journalEntry.details,
+                                maxLines: 3,
+                                validator: (String value) {
+                                  if (value.isEmpty) {
+                                    return 'Enter details';
+                                  }
+                                  return null;
+                                },
+                                onSaved: (String value) {
+                                  _journalEntry.details = value;
+                                },
+                              ),
+                              Row(
+                                children: <Widget>[
+                                  Padding(
+                                      padding: const EdgeInsets.fromLTRB(10.0, 5.0, 8.0, 5.0),
+                                      child: RaisedButton(
+                                          onPressed: () {
+                                            Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) => MentalDistortions(
+                                                      notifyParent: _updateDistortions,
+                                                      distortionIds: _journalEntry.distortions)),
+                                            );
+                                          },
+                                          child: Text('Mental Distortions'))),
+                                  Text('${_journalEntry.distortions.length.toString()} selected')
+                                ],
+                              ),
+                              Row(
+                                children: <Widget>[
+                                  Padding(
+                                      padding: const EdgeInsets.fromLTRB(10.0, 5.0, 8.0, 5.0),
+                                      child: RaisedButton(
+                                          onPressed: () {
+                                            Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      HALT(notifyParent: _updateHALT, halt: _journalEntry.halt)),
+                                            );
+                                          },
+                                          child: Text('HALT'))),
+                                  Text(
+                                      '${(Map.from(_journalEntry.halt)..removeWhere((k, v) => v == false)).length.toString()} selected')
+                                ],
+                              ),
+                              CustomTextFormField(
+                                hintText: 'Rational Thought',
+                                value: _journalEntry.rationalThought,
+                                maxLines: 3,
+                                validator: (String value) {
+                                  if (value.isEmpty) {
+                                    return 'Enter rational thought';
+                                  }
+                                  return null;
+                                },
+                                onSaved: (String value) {
+                                  _journalEntry.rationalThought = value;
+                                },
+                              ),
+                              RaisedButton(
+                                  onPressed: () {
+                                    if (_formKey.currentState.validate()) {
+                                      _formKey.currentState.save();
+                                      _journalEntry.date = DateTime.now();
+                                      widget.notifyParent(_journalEntry);
+                                      Navigator.pop(context);
+                                    }
+                                  },
+                                  child: Text(_journalEntry.id == null ? 'Add New Entry' : 'Finish Editing'))
                             ],
-                          ),
-                          CustomTextFormField(
-                            hintText: 'Details',
-                            value: _journalEntry.details,
-                            maxLines: 3,
-                            validator: (String value) {
-                              if (value.isEmpty) {
-                                return 'Enter details';
-                              }
-                              return null;
-                            },
-                            onSaved: (String value) {
-                              _journalEntry.details = value;
-                            },
-                          ),
-                          Row(
-                            children: <Widget>[
-                              Padding(
-                                  padding: const EdgeInsets.fromLTRB(10.0, 5.0, 8.0, 5.0),
-                                  child: RaisedButton(
-                                      onPressed: () {
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) => MentalDistortions(
-                                                  notifyParent: updateDistortions,
-                                                  distortionIds: _journalEntry.distortions)),
-                                        );
-                                      },
-                                      child: Text('Mental Distortions'))),
-                              Text('${_journalEntry.distortions.length.toString()} selected')
-                            ],
-                          ),
-                          Row(
-                            children: <Widget>[
-                              Padding(
-                                  padding: const EdgeInsets.fromLTRB(10.0, 5.0, 8.0, 5.0),
-                                  child: RaisedButton(
-                                      onPressed: () {
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) =>
-                                                  HALT(notifyParent: updateHALT, halt: _journalEntry.halt)),
-                                        );
-                                      },
-                                      child: Text('HALT'))),
-                              Text(
-                                  '${(Map.from(_journalEntry.halt)..removeWhere((k, v) => v == false)).length.toString()} selected')
-                            ],
-                          ),
-                          CustomTextFormField(
-                            hintText: 'Rational Thought',
-                            value: _journalEntry.rationalThought,
-                            maxLines: 3,
-                            validator: (String value) {
-                              if (value.isEmpty) {
-                                return 'Enter rational thought';
-                              }
-                              return null;
-                            },
-                            onSaved: (String value) {
-                              _journalEntry.rationalThought = value;
-                            },
-                          ),
-                          RaisedButton(
-                              onPressed: () {
-                                if (_formKey.currentState.validate()) {
-                                  _formKey.currentState.save();
-                                  _journalEntry.date = DateTime.now();
-                                  widget.notifyParent(_journalEntry);
-                                  Navigator.pop(context);
-                                }
-                              },
-                              child: Text('Add New Entry'))
-                        ],
-                      )),
-                ))));
+                          )),
+                    )))));
   }
 }
 
